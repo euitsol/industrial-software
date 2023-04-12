00ddf38ece1f69009a2689cb48af9ef458dac834
Sandeep Chowdhury <116838065+mrsharifull@users.noreply.github.com>
2023-04-12 15:00:56 +0600

    Student Profile> Job Placement --done

--- software_actual/app/Http/Controllers/AuthStudent/StudentController.php
+++ software_actual/app/Http/Controllers/AuthStudent/StudentController.php
@@ -8,6 +8,7 @@ use App\Models\BatchAttendance;
 use App\Models\JobPlacement;
 use App\Models\LinkageIndustryInfo;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 use App\Http\Controllers\Controller;
 
 class StudentController extends Controller
@@ -80,21 +81,77 @@ class StudentController extends Controller
     public function JPstore(Request $request)
     {
         $request->validate([
-            'linkage_industry_info_id' => 'required',
             'designation' => 'required|max:255',
             'joining_date' => 'required|date',
         ]);
+        if (!isset($request->company_name)){
+            $request->validate([
+                'linkage_industry_info_id' => 'required',
+            ]);
+        }
+        if (empty($request->linkage_industry_info_id) && empty($request->company_name)) {
+            $this->message('error', 'Please select or add new company info.');
+            return redirect()->back()->withInput();
+        }
+        if (!empty($request->linkage_industry_info_id) && !empty($request->company_name)) {
+            $this->message('error', 'Company has both items data not applicable.');
+            return redirect()->back()->withInput();
+        }
+
+        if (isset($request->company_name)) {
+            if (empty($request->company_name) || empty($request->company_logo) || empty($request->company_website) || empty($request->company_address) || empty($request->contact_person_name) || empty($request->contact_number) || empty($request->contact_email) || empty($request->description)) {
+                // $this->message('error', 'All field required!.');
+                $request->validate([
+                    'company_name' => 'required|max:255',
+                    'company_logo' => 'image|mimes:jpg,jpeg,png,JPG,JPEG,PNG|max:5120',
+                    'company_website' => 'required|max:255',
+                    'company_address' => 'required|max:255',
+                    'contact_person_name' => 'required|max:255',
+                    'contact_number' => 'required',
+                    'contact_email' => 'required',
+                    'description' => 'required|max:10000',
+                ]);
+                return redirect()->back()->withInput();
+            }
+        }
 
             $m = new JobPlacement;
             $m->student_id = $request->student_id;
-            $m->linkage_industry_info_id = $request->linkage_industry_info_id;
+
+            if (empty($request->linkage_industry_info_id) && !empty($request->company_name)) {
+                $data = new LinkageIndustryInfo;
+                $data->company_name = $request->company_name;
+
+                if ($request->hasFile('company_logo')) {
+                    $logo = $request->company_logo;
+                    $img_name = time() . '_' . $logo->getClientOriginalName();
+                    $logo->move('uploads/images/', $img_name);
+                    $data->company_logo = 'uploads/images/' . $img_name;
+                    
+                }
+
+                $data->company_website = $request->company_website;
+                $data->company_address = $request->company_address;
+                $data->contact_person_name = $request->contact_person_name;
+                $data->contact_number = $request->contact_number;
+                $data->contact_email = $request->contact_email;
+                $data->description = $request->description;
+                $data->created_by = 35;
+                $data->created_at = Carbon::now();
+                $data->save();
+                
+                $id = LinkageIndustryInfo::latest()->first();
+                $m->linkage_industry_info_id = $id->id;
+            } else {
+                $m->linkage_industry_info_id = $request->linkage_industry_info_id;
+            }
             $m->designation = $request->designation;
             if (isset($request->department))
             {
                 $m->department = $request->department;
             }
             $m->joining_date = $request->joining_date;
-            $m->created_by = auth()->user()->id;
+            $m->created_by = 35;
             $m->created_at = Carbon::now();
             $m->save();
 
@@ -128,7 +185,7 @@ class StudentController extends Controller
                 $m->department = $request->department;
             }
             $m->joining_date = $request->joining_date;
-            $m->updated_by = auth()->user()->id;
+            $m->updated_by = 35;
             $m->updated_at = Carbon::now();
             $m->update();
 
--- software_actual/resources/views/student_panel/job_placement/create.blade.php
+++ software_actual/resources/views/student_panel/job_placement/create.blade.php
@@ -2,6 +2,15 @@
 
 @section('title', 'Create Job Placement - European IT Solutions Institute')
 
+@push('css')
+    <link rel="stylesheet" href="{{ asset('assets/vendor/select2/select2.min.css') }}">
+    <style>
+        .select2 {
+            width: 100% !important;
+        }
+    </style>
+@endpush
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -17,31 +26,125 @@
                 </div>
 
                 <div class="card-body">
+
+                    @if(session('success'))
+                        <p class="alert alert-success text-center">
+                            {{ session('success') }}
+                        </p>
+                    @elseif(session('error'))
+                        <p class="alert alert-danger text-center">
+                            {{ session('error') }}
+                        </p>
+                    @endif
                     
                     <div class="row">
                         <div class="col-md-8 offset-md-2">
-                            <form action="{{ route('student.job_placement.store') }}" method="POST" class="form-horizontal">
+                            <form action="{{ route('student.job_placement.store') }}" method="POST" class="form-horizontal" enctype="multipart/form-data">
                                 @csrf
 
                                 <input type="hidden" name="student_id" value="{{ $student_id }}">
-                                <div class="form-group row">
-                                    <label class="col-md-3 form-control-label">Company Name <span class="text-danger">*</span> </label>
-                                    <div class="col-md-9">
-                                        <select name="linkage_industry_info_id" class="form-control form-control-success" required>
-                                            <option selected disabled value="">Choose...</option>
-                                            @foreach ($linkage_industries as $linkage_industry)
-                                                <option value="{{ $linkage_industry->id }}">{{ $linkage_industry->company_name }}</option>
-                                            @endforeach
-                                        </select>
-                                        @if ($errors->has('linkage_industry_info_id'))
-                                            <span class="text-danger">{{ $errors->first('linkage_industry_info_id') }}</span>
-                                        @endif
+
+
+
+                                <div id="company_wrapper">
+                                    <div class="form-group row">
+                                        <label class="col-md-3 form-control-label">Company Name <span class="text-danger">*</span> </label>
+                                        <div class="col-md-9">
+                                            <select name="linkage_industry_info_id" id="company" class="form-control form-control-success">
+                                                <option selected value="">Choose...</option>
+                                                @foreach ($linkage_industries as $linkage_industry)
+                                                    <option value="{{ $linkage_industry->id }}">{{ $linkage_industry->company_name }}</option>
+                                                @endforeach
+                                            </select>
+                                            @if ($errors->has('linkage_industry_info_id'))
+                                                <span class="text-danger">{{ $errors->first('linkage_industry_info_id') }}</span>
+                                            @endif
+                                            <div class="clearfix">
+                                                <small>If company not exist then you can add</small>
+                                                <a href="javascript:void(0)" id="new_company" class="float-right">
+                                                    + New Company
+                                                </a>
+                                            </div>
+                                            <div id="new_company_form" class="border p-2" style="display:none">
+                                                <div class="form-group">
+                                                    <label class="form-control-label">Company Name <span
+                                                                class="text-danger">*</span> </label>
+                                                    <input type="text" name="company_name"
+                                                        value="{{ old('company_name') }}"
+                                                        class="form-control form-control-sm">
+                                                    @if ($errors->has('company_name'))
+                                                        <span class="text-danger">{{ $errors->first('company_name') }}</span>
+                                                    @endif
+                                                </div>
+                                                
+                                                <div class="form-group">
+                                                    <label class="form-control-label">Company Logo <span
+                                                        class="text-danger">*</span></label>
+                                                    <div id="image-preview"></div>
+                                                    <input type="file" name="company_logo" id="photo"
+                                                        value="{{ old('company_logo') }}"
+                                                        class="form-control form-control-sm">
+                                                    @if ($errors->has('company_logo'))
+                                                        <span class="text-danger">{{ $errors->first('company_logo') }}</span>
+                                                    @endif
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="form-control-label">Company Website URL <span
+                                                                class="text-danger">*</span> </label>
+                                                    <input type="text" name="company_website" value="{{ old('company_website') }}"
+                                                        class="form-control form-control-sm">
+                                                    @if ($errors->has('company_website'))
+                                                        <span class="text-danger">{{ $errors->first('company_website') }}</span>
+                                                    @endif
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="form-control-label">Company Address <span class="text-danger">*</span> </label>
+                                                    <input type="text" name="company_address" value="{{ old('company_address') }}"
+                                                        class="form-control form-control-sm">
+                                                    @if ($errors->has('company_address'))
+                                                        <span class="text-danger">{{ $errors->first('company_address') }}</span>
+                                                    @endif
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="form-control-label">Contact Person Name <span class="text-danger">*</span></label>
+                                                    <input type="text" name="contact_person_name" value="{{ old('contact_person_name') }}"
+                                                        class="form-control form-control-sm">
+                                                    @if ($errors->has('contact_person_name'))
+                                                        <span class="text-danger">{{ $errors->first('contact_person_name') }}</span>
+                                                    @endif
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="form-control-label">Contact Number <span class="text-danger">*</span></label>
+                                                    <input type="number" name="contact_number" value="{{ old('contact_number') }}"
+                                                        class="form-control form-control-sm">
+                                                    @if ($errors->has('contact_number'))
+                                                        <span class="text-danger">{{ $errors->first('contact_number') }}</span>
+                                                    @endif
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="form-control-label">Contact Email <span class="text-danger">*</span></label>
+                                                    <input type="email" name="contact_email" value="{{ old('contact_email') }}"
+                                                        class="form-control form-control-sm">
+                                                    @if ($errors->has('contact_email'))
+                                                        <span class="text-danger">{{ $errors->first('contact_email') }}</span>
+                                                    @endif
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="form-control-label">Short Description <span class="text-danger">*</span></label>
+                                                    <textarea name="description" id="description"
+                                                    class="form-control form-control-sm">{{ old('description') }}</textarea>
+                                                    @if ($errors->has('description'))
+                                                        <span class="text-danger">{{ $errors->first('description') }}</span>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="form-group row">
                                     <label class="col-md-3 form-control-label">Designation <span class="text-danger">*</span> </label>
                                     <div class="col-md-9">
-                                        <input type="text" name="designation" value="{{ old('designation') }}" id="designation" class="form-control form-control-success" required>
+                                        <input type="text" name="designation" value="{{ old('designation') }}" id="designation" class="form-control form-control-success">
                                         @if ($errors->has('designation'))
                                             <span class="text-danger">{{ $errors->first('designation') }}</span>
                                         @endif
@@ -59,7 +162,7 @@
                                 <div class="form-group row">
                                     <label class="col-md-3 form-control-label">Joining Date <span class="text-danger">*</span></label>
                                     <div class="col-md-9">
-                                        <input type="date" name="joining_date" value="{{ old('joining_date') }}" id="joining_date" class="form-control form-control-success" required>
+                                        <input type="date" name="joining_date" value="{{ old('joining_date') }}" id="joining_date" class="form-control form-control-success">
                                         @if ($errors->has('joining_date'))
                                             <span class="text-danger">{{ $errors->first('joining_date') }}</span>
                                         @endif
@@ -118,6 +221,8 @@
 @endsection
 
 @push('js')
+    <script src="{{ asset('assets/vendor/select2/select2.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/ckeditor/ckeditor.js') }}"></script>
     <script>
 
         title_short_form();
@@ -142,5 +247,27 @@
                 title_short.prop('disabled', true);
             }
         }
+    </script>
+    <script>
+        $(document).on('change', '#photo', function (e) {
+                const file = e.target.files[0] || e.dataTransfers.files[0];
+                let reader = new FileReader();
+                reader.onload = function (ev) {
+                    let img = document.createElement('img');
+                    img.height = '100';
+                    img.width = '100';
+                    img.src = ev.target.result;
+                    img.style.marginRight = '5px';
+                    img.style.marginBottom = '5px';
+                    $('#image-preview').html(img);
+                };
+                reader.readAsDataURL(file);
+            });
+    </script>
+    <script>
+        $("#company").select2();
+        $("#new_company").click(function () {
+            $("#new_company_form").toggle(500);
+        });
     </script>
 @endpush
