a8c7be75a05d370c7176f446a1be9fcb87ead02a
Md Shariful Islam <mdsharifulislamm.info@gmail.com>
2024-06-27 11:01:41 +0600

    New Payment discount access auth check --done

--- software_actual/resources/views/account/new_payment.blade.php
+++ software_actual/resources/views/account/new_payment.blade.php
@@ -8,13 +8,13 @@
             <div class="col-md-10 col-lg-12">
                 <div class="card">
                     <div class="card-header">
-                    <span class="float-left">
-                        <h4>Payment</h4>
-                    </span>
+                        <span class="float-left">
+                            <h4>Payment</h4>
+                        </span>
                         <span class="float-right">
-                        <a href="{{ route('account.student.courses', $student->id) }}"
-                           class="btn btn-info btn-sm">Back</a>
-                    </span>
+                            <a href="{{ route('account.student.courses', $student->id) }}"
+                                class="btn btn-info btn-sm">Back</a>
+                        </span>
                     </div>
 
                     <div class="card-body">
@@ -22,7 +22,7 @@
                         <div class="row">
                             <div class="col-md-8">
 
-                                @if(session('success'))
+                                @if (session('success'))
                                     <p class="alert alert-success text-center">
                                         {{ session('success') }}
                                     </p>
@@ -46,42 +46,47 @@
                                     <input type="hidden" name="student_id" value="{{ $student->id }}">
                                     <input type="hidden" name="course_id" value="{{ $course->id }}">
                                     <input type="hidden" name="course_fee" value="{{ $course->fee }}">
-                                    <input type="hidden" name="batch_name" value="{{batch_name($course->title_short_form, $batch->year, $batch->month, $batch->batch_number)}}">
-                                    
+                                    <input type="hidden" name="batch_name"
+                                        value="{{ batch_name($course->title_short_form, $batch->year, $batch->month, $batch->batch_number) }}">
+
                                     <div class="form-group row">
                                         <label class="col-md-3 form-control-label">Course Fee</label>
-                                        <div class="col-md-9">{{$course->fee}} TK</div>
+                                        <div class="col-md-9">{{ $course->fee }} TK</div>
                                     </div>
-
+                                    @php
+                                        $authCheck =
+                                            Auth::user()->role == 'admin' || Auth::user()->role == 'superadmin';
+                                    @endphp
                                     <div class="form-group row">
-                                        <label class="col-md-3 form-control-label">Discount</label>
+                                        @if ($authCheck)
+                                            <label class="col-md-3 form-control-label">Discount</label>
+                                        @endif
                                         <div class="col-md-9">
+                                            @if ($authCheck)
+                                                <label for="discount_yes" class="mr-2">
+                                                    <input type="radio" name="discount_radio" id="discount_yes"
+                                                        value="yes">
+                                                    Yes</label>
 
-                                            <label for="discount_yes" class="mr-2">
-                                                <input type="radio" name="discount_radio" id="discount_yes" value="yes">
-                                                Yes</label>
-
-                                            <label for="discount_no">
-                                                <input type="radio" name="discount_radio" id="discount_no" value="no"
-                                                       checked> No</label>
+                                                <label for="discount_no">
+                                                    <input type="radio" name="discount_radio" id="discount_no"
+                                                        value="no" checked> No</label>
+                                            @endif
 
                                             <div class="input-group" id="discount" style="display: none">
 
                                                 <input type="number" name="discount_percent" id="discount_percent"
-                                                       value="" min="0"
-                                                       placeholder="Percent"
-                                                       class="form-control">
+                                                    value="" min="0" placeholder="Percent"
+                                                    class="form-control">
 
                                                 <div class="input-group-append">
-                                                    <span class="input-group-text"
-                                                          style="width: 36px !important;">%</span>
+                                                    <span class="input-group-text" style="width: 36px !important;">%</span>
                                                 </div>
 
                                                 <span class="ml-2 mr-2 mt-2">OR</span>
 
                                                 <input type="number" name="discount_amount" id="discount_amount"
-                                                       value="" min="0" placeholder="Amount"
-                                                       class="form-control">
+                                                    value="" min="0" placeholder="Amount" class="form-control">
                                             </div>
 
                                         </div>
@@ -101,11 +106,11 @@
 
                                             <label for="method_installment" class="mr-2">
                                                 <input type="radio" name="method_radio" id="method_installment"
-                                                       value="installment" checked> Installment</label>
+                                                    value="installment" checked> Installment</label>
 
                                             <label for="method_fullPayment">
                                                 <input type="radio" name="method_radio" id="method_fullPayment"
-                                                       value="full_payment" > Full Payment</label>
+                                                    value="full_payment"> Full Payment</label>
                                         </div>
                                     </div>
 
@@ -114,11 +119,12 @@
                                         <div class="col-md-9">
                                             <input type="checkbox" name="payment_type[]" id="type_cash" value="cash">
                                             <label for="type_cash" class="mr-2">Cash</label>
-                                            <input type="checkbox" name="payment_type[]" id="type_mb" value="mobile_banking" >
+                                            <input type="checkbox" name="payment_type[]" id="type_mb"
+                                                value="mobile_banking">
                                             <label for="type_mb">Mobile Banking</label>
                                             <br>
                                             @if ($errors->has('payment_type'))
-                                                <span class="text-danger">{{$errors->first('payment_type')}}</span>
+                                                <span class="text-danger">{{ $errors->first('payment_type') }}</span>
                                             @endif
                                         </div>
                                     </div>
@@ -127,18 +133,25 @@
                                         <div class="col-md-3"></div>
                                         <div class="col-md-9" id="payment_inputs">
                                             <div class="input-group w-50 mt-2" id="cash_container" style="display:none;">
-                                                <input placeholder="Cash payment amount" type="number" name="cash_payment" id="cash_payment" value="" min="0"class="form-control form-control-sm">
+                                                <input placeholder="Cash payment amount" type="number"
+                                                    name="cash_payment" id="cash_payment" value=""
+                                                    min="0"class="form-control form-control-sm">
                                             </div>
                                             <div class="w-50 mt-2" id="mb_container" style="display:none;">
-                                                <select class="form-control form-control-sm" name="mb_type" id="mb_type">
+                                                <select class="form-control form-control-sm" name="mb_type"
+                                                    id="mb_type">
                                                     <option value="" hidden>Banking Type</option>
                                                     <option value="1">Bkash</option>
                                                     <option value="2">Rocket</option>
                                                     <option value="3">Nagad</option>
                                                     <option value="4">Nexus Pay</option>
                                                 </select>
-                                                <input placeholder="MB payment amount" type="number" name="mb_payment" id="mb_payment" value="" min="0" class="form-control form-control-sm mt-2">
-                                                <input placeholder="Trxn ID Last 6 digits (Uppercase)" type="text" name="mb_trxn" id="mb_trxn" value="" class="form-control form-control-sm mt-2">
+                                                <input placeholder="MB payment amount" type="number" name="mb_payment"
+                                                    id="mb_payment" value="" min="0"
+                                                    class="form-control form-control-sm mt-2">
+                                                <input placeholder="Trxn ID Last 6 digits (Uppercase)" type="text"
+                                                    name="mb_trxn" id="mb_trxn" value=""
+                                                    class="form-control form-control-sm mt-2">
                                             </div>
                                         </div>
                                     </div>
@@ -147,10 +160,10 @@
                                         <label for="" class="col-md-3 form-control-label">Total Amount </label>
                                         <div class="col-md-9">
                                             <input type="text" name="amount" id="today_payable"
-                                                   class="form-control w-50" disabled>
+                                                class="form-control w-50" disabled>
                                             <input type="hidden" name="amount" id="payable_amount" class="d-none">
                                             @if ($errors->has('amount'))
-                                                <span class="text-danger">{{$errors->first('amount')}}</span>
+                                                <span class="text-danger">{{ $errors->first('amount') }}</span>
                                             @endif
                                         </div>
                                     </div>
@@ -160,8 +173,7 @@
                                         <div class="col-md-9">
                                             <div class="input-group w-50">
                                                 <input type="number" name="installment_quantity"
-                                                    id="installment_quantity"
-                                                    value="" min="0"
+                                                    id="installment_quantity" value="" min="0"
                                                     placeholder="How many installment"
                                                     class="form-control form-control-sm">
                                             </div>
@@ -184,7 +196,7 @@
                                     <tr>
                                         <td>Student ID</td>
                                         <td>:</td>
-                                        <td>{{ $student->year.$student->reg_no }}</td>
+                                        <td>{{ $student->year . $student->reg_no }}</td>
                                     </tr>
                                     <tr>
                                         <td>Name</td>
@@ -199,12 +211,13 @@
                                     <tr>
                                         <td>Course</td>
                                         <td>:</td>
-                                        <td>{{$course->title}}</td>
+                                        <td>{{ $course->title }}</td>
                                     </tr>
                                     <tr>
                                         <td>Batch</td>
                                         <td>:</td>
-                                        <td>{{batch_name($course->title_short_form, $batch->year, $batch->month, $batch->batch_number)}}</td>
+                                        <td>{{ batch_name($course->title_short_form, $batch->year, $batch->month, $batch->batch_number) }}
+                                        </td>
                                     </tr>
                                     <tr>
                                         <td>Phone</td>
@@ -215,7 +228,8 @@
                                         <tr>
                                             <td>Photo</td>
                                             <td>:</td>
-                                            <td><img src="{{asset($student->photo)}}" style="height: 120px;" alt="">
+                                            <td><img src="{{ asset($student->photo) }}" style="height: 120px;"
+                                                    alt="">
                                             </td>
                                         </tr>
                                     @endif
@@ -234,22 +248,22 @@
     <script>
         let $course_fee = ("{{ $course->fee }}" * 1);
 
-        $("#discount_yes").click(function () {
+        $("#discount_yes").click(function() {
             $("#discount").show(100);
             payment_calculation();
         });
-        $("#discount_no").click(function () {
+        $("#discount_no").click(function() {
             $("#discount_percent").val('');
             $("#discount_amount").val('');
             $("#discount").hide(100);
             $('#total_fee').html($course_fee);
             payment_calculation();
         });
-        $("#method_installment").click(function () {
+        $("#method_installment").click(function() {
             $("#payment_method").show(100);
             payment_calculation();
         });
-        $("#method_fullPayment").click(function () {
+        $("#method_fullPayment").click(function() {
             $("#installment_quantity").val('');
             $("#payment_method").hide(100);
             payment_calculation();
@@ -263,7 +277,7 @@
         let total = 0;
 
         function payment_calculation() {
-            let given_actual_total =  $('#today_payable').val();
+            let given_actual_total = $('#today_payable').val();
             let split_total = 0;
             let discount_percent = $("#discount_percent");
             let discount_percent_val = $("#discount_percent").val();
@@ -299,7 +313,7 @@
                 total = total - discount_amount_val;
                 total_fee = total_fee - discount_amount_val;
             }
-            if((parseInt($('#cash_payment').val()) || 0) > 0 || (parseInt($('#mb_payment').val()) || 0) > 0){
+            if ((parseInt($('#cash_payment').val()) || 0) > 0 || (parseInt($('#mb_payment').val()) || 0) > 0) {
                 total = given_actual_total;
             }
             if ((installment_quantity_val * 1) > 0) {
@@ -314,11 +328,12 @@
 
         payment_calculation();
 
-        $(document).on('keyup change focusout input', "#discount_percent, #discount_amount, #installment_quantity", function () {
-            payment_calculation();
-        });
+        $(document).on('keyup change focusout input', "#discount_percent, #discount_amount, #installment_quantity",
+            function() {
+                payment_calculation();
+            });
 
-        $(document).on('keyup change focusout input', '#today_payable', function () {
+        $(document).on('keyup change focusout input', '#today_payable', function() {
             installmentDatesGenerate($('#total_fee').text(), ($(this).val() * 1));
         });
 
@@ -327,10 +342,12 @@
             if ('' !== installment_quantity_val && installment_quantity_val > 0) {
                 let installment_fee_total = _total_fee - _total;
                 let output = '<table class="mt-4 table table-bordered">';
-                output += '<tr><td rowspan="' + (parseInt(installment_quantity_val) + 1) + '" class="gray">Installment Dates</td></tr>';
+                output += '<tr><td rowspan="' + (parseInt(installment_quantity_val) + 1) +
+                    '" class="gray">Installment Dates</td></tr>';
                 let installment_fee = installment_fee_total / parseInt(installment_quantity_val);
                 for (let i = 0; i < parseInt(installment_quantity_val); i++) {
-                    output += '<tr><td><input type="date" name="installment_date[]" class="form-control form-control-sm" value=""></td>';
+                    output +=
+                        '<tr><td><input type="date" name="installment_date[]" class="form-control form-control-sm" value=""></td>';
                     output += '<td>' + Math.ceil(installment_fee) + '</td></tr>';
                 }
                 output += '</table>';
@@ -354,7 +371,6 @@
         function end(array) {
             return array[array.length - 1];
         }
-
     </script>
     <script>
         $(document).ready(function() {
@@ -384,6 +400,5 @@
                 payment_calculation();
             });
         });
-    
     </script>
 @endpush
